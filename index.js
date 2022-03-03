0eae14fb3243533266a20e38d324dc59cd2170e4
diegoavalosf <diego_avalos@live.com.mx>
2022-03-02 19:13:14 -0600

    update get & post

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const {pokemon}= require('./pokedex.json');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res, next)=>{
-    res.status(200);
-    res.send("Welcome to Pokidex");
+    return res.status(200).send("Welcome to Pokidex");
 });
 
-app.get("/pokemon/all", (req, res, next)=>{
-    res.status(200);
-    res.send(pokemon);
+app.post("/pokemon", (req, res, next)=>{
+    return res.status(200).send(req.body);
+});
+
+app.get("/pokemon", (req, res, next)=>{
+    
+    return res.status(200).send(pokemon);
 });
 
 app.get("/pokemon/:id([0-9]{1,3})", (req, res, next)=>{
@@ -19,20 +26,15 @@ app.get("/pokemon/:id([0-9]{1,3})", (req, res, next)=>{
         res.status(200);
         return res.send(pokemon[req.params.id-1]);
     }
-    res.status(404);
-    res.send("Error404: pokimon not found")
-});
+    return res.status(404).send("Error404: pokimon not found")
+}); 
 
-app.get("/pokemon/:name", (req, res, next)=>{
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next)=>{
     const name = req.params.name;
-    for (i = 0; i < pokemon.length; i++){
-        if (pokemon[i].name == name){
-            res.status(200);    
-            return res.send(pokemon[i]);
-        }
-    }
-    res.status(404);
-    res.send("Error404: pokimon not found");
+    const pk = pokemon.filter((p) =>{
+        return (p.name.toUpperCase() == name.toUpperCase())? p: null; 
+    });
+    return (pk.length > 0)? res.status(200).send(pk): res.status(404).send("Error404: pokimon not found");
 });
 
 app.listen (process.env.PORT || 3000, ()=> {
