7745da28b9c210a96d05d59d9a109cb12056b498
diegoavalosf <diego_avalos@live.com.mx>
2022-03-09 19:03:59 -0600

    add json responses

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const bodyParser = require('body-parser');
+
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const pokemon = require('./routes/pokemon');
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 app.get("/", (req, res, next)=>{
-    return res.status(200).send("Welcome to Pokidex");
+    return res.status(200).json({code: 1, message: "Welcome to Pokidex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use ((req, res, next) =>{
+    return res.status(404).json({code: 404, message: "URL not found"})
+});
 
 app.listen (process.env.PORT || 3000, ()=> {
     console.log("Server is running...");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const pokemon = express.Router();
-const db = require('../config/database')
+const db = require('../config/database');
 
 pokemon.post("/", (req, res, next)=>{
     return res.status(200).send(req.body);
@@ -9,26 +9,25 @@ pokemon.post("/", (req, res, next)=>{
 
 pokemon.get("/", async (req, res, next)=>{
     const kuery = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json(kuery);
+    return res.status(200).json({code: 1, message: kuery});
 });
 
 pokemon.get("/:id([0-9]{1,3})", async (req, res, next)=>{
     const id = req.params.id;
-    if(id > 0 && id <= 722){
+    if(id > 1 && id <= 722){
         const kuery = await db.query("SELECT * FROM pokemon WHERE pok_id = '"+id+"'");
-        return res.status(200).json(kuery);
+        return res.status(200).json({code: 1, message: kuery});
     }
-    return res.status(404).send("Error404: pokimon id not found")
+    return res.status(404).send({code: 1, message: "Error404: pokimon id not found"})
 }); 
 
 pokemon.get("/:name([A-Za-z]+)", async (req, res, next)=>{
     const name = req.params.name;
     const kuery = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name+"'");
-    console.log(kuery.lenght)
-    if (kuery == undefined){
-        return res.status(400).send("Error404: pokimon name not found");
+    if (kuery.length > 0){
+        return res.status(200).json({code: 1, message: kuery});
     }
-    return res.status(200).json(kuery);
+    return res.status(400).send({code: 1, message: "Error404: pokimon name not found"});
 });
 
 module.exports = pokemon;
