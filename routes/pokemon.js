90f23ec22f6edbb50445cc035b0d731e7cb11f60
diegoavalosf <diego_avalos@live.com.mx>
2022-03-14 19:33:45 -0600

    added post

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,31 +3,45 @@ const express = require('express');
 const pokemon = express.Router();
 const db = require('../config/database');
 
-pokemon.post("/", (req, res, next)=>{
-    return res.status(200).send(req.body);
+pokemon.post("/", async(req, res, next)=>{
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+
+    if(pok_name && pok_height && pok_weight && pok_base_experience){
+        let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+
+        const rows = await db.query(query);
+        console.log(rows);
+
+        if(rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "Pokimon inserted correctly"})
+        }
+        return res.status(500).json({code: 500, message: "Error:("});
+    }
+    return res.status(500).json({code:500, message:"Error: Empty fields"})
 });
 
 pokemon.get("/", async (req, res, next)=>{
     const kuery = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({code: 1, message: kuery});
+    return res.status(200).json({code: 200, message: kuery});
 });
 
 pokemon.get("/:id([0-9]{1,3})", async (req, res, next)=>{
     const id = req.params.id;
     if(id > 1 && id <= 722){
         const kuery = await db.query("SELECT * FROM pokemon WHERE pok_id = '"+id+"'");
-        return res.status(200).json({code: 1, message: kuery});
+        return res.status(200).json({code: 200, message: kuery});
     }
-    return res.status(404).send({code: 1, message: "Error404: pokimon id not found"})
+    return res.status(404).send({code: 404, message: "Error404: pokimon id not found"})
 }); 
 
 pokemon.get("/:name([A-Za-z]+)", async (req, res, next)=>{
     const name = req.params.name;
     const kuery = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name+"'");
     if (kuery.length > 0){
-        return res.status(200).json({code: 1, message: kuery});
+        return res.status(200).json({code: 200, message: kuery});
     }
-    return res.status(400).send({code: 1, message: "Error404: pokimon name not found"});
+    return res.status(400).send({code: 404, message: "Error404: pokimon name not found"});
 });
 
 module.exports = pokemon;
